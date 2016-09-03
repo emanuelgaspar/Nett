d9d9e7097abbafa32ed381a91ab9687455132870
paiden <paiden@outlook.com>
2016-09-03 21:40:22 +0200

    Add config sources concept

--- Source/Nett/TomlObject.cs
+++ Source/Nett/TomlObject.cs
@@ -5,7 +5,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
-    using Extensions;
+    using LinqExtensions;
 
     public abstract class TomlObject
     {
